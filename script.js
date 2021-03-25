ababc05b9895a4ceb842967ca55657870027b8c5
R.Aldie <rioaldi@gmail.com>
2021-03-25 17:12:09 +0700

    initila commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hari = document.querySelectorAll('.hari');
+hari[0].style.color ="black";
+hari[1].style.color ="black";
+hari[2].style.color ="black";
+hari[3].style.color ="black";
+hari[4].style.color ="black";
